86662ae52efd0fd8d4428bdfd9cb63797b04f07c
Ben Greaves <bsgreaves@gmail.com>
2017-02-28 19:34:53 -0600

    Cleaned up JS. Ready for Feedback

--- cards.js
+++ cards.js
@@ -1,36 +1,28 @@
 var userInputField = document.getElementById("userInput");
 var createButton = document.getElementById("createButton");
 var cardHolder = document.getElementById("cardHolder");
-var cardCount = 0;
 
-// var test = function () {
-// 	var testVar = `card ${cardCount}`;
-// 	console.log("testvar", testVar);
-// 	cardCount++;
-// }
-
-var createCard = function () {
-	console.log("createCard fired")
-	var newCard = document.createElement("div");
-	cardCount++;
-	newCard.className = "card";
-	var content = document.createTextNode(userInputField.value);
-	newCard.appendChild(content);
-	var deleteBtn = document.createElement("button");
-	deleteBtn.className = "deleteButton";
-	deleteBtn.innerHTML = "Delete"
-	deleteBtn.addEventListener("click", deleteCard)
-	newCard.appendChild(deleteBtn);
-	cardHolder.appendChild(newCard);
-	console.log("createCard ended")
+var createCard = function() {
+    console.log("createCard fired")
+    var newCard = document.createElement("div");
+    cardCount++;
+    newCard.className = "card";
+    var content = document.createTextNode(userInputField.value);
+    newCard.appendChild(content);
+    var deleteBtn = document.createElement("button");
+    deleteBtn.className = "deleteButton";
+    deleteBtn.innerHTML = "Delete"
+    deleteBtn.addEventListener("click", deleteCard)
+    newCard.appendChild(deleteBtn);
+    cardHolder.appendChild(newCard);
+    console.log("createCard ended")
 }
 
-var deleteCard = function (e) {
-
-	var currCard = e.target.parentNode;
-	console.log(currCard);
-	cardHolder.removeChild(currCard);
-	console.log("deleteCard fired")
+var deleteCard = function(e) {
+    var currCard = e.target.parentNode;
+    console.log(currCard);
+    cardHolder.removeChild(currCard);
+    console.log("deleteCard fired")
 }
 
 createButton.addEventListener("click", createCard);
